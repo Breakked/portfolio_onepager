6871d70e257a597674f02fc90be09afcd2c6b129
Andri <andricsetreki@hotmail.com>
2019-11-08 09:16:05 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,8 +1,12 @@
 /* Canvas-Cube auf der Startseite */
 
-var c = document.getElementById("cube");
-var ctx = c.getContext("2d");
-//Füllfarbe festlegen
-ctx.fillStyle = "yellow";
-//Rechteck mit Füllfarbe zeichnen
-ctx.fillRect(20,20,300,150);
+
+    var canvas = document.getElementById("cube");
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+  
+      ctx.fillRect(25, 25, 100, 100);
+      ctx.clearRect(45, 45, 60, 60);
+      ctx.strokeRect(50, 50, 50, 50);
+    }
+  
