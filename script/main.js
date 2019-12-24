a77f425a7ecc59de17f46352c2926650d5f3e9f5
Andri <andricsetreki@hotmail.com>
2019-12-24 15:44:02 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,23 +1,36 @@
-window.addEventListener('scroll', function() {
-    document.getElementById('showScroll').innerHTML = window.pageYOffset + 'px';
-  });
+window.addEventListener('scroll', function () {
+  document.getElementById('showScroll').innerHTML = window.pageYOffset + 'px';
+});
 
 
-window.onscroll = function() {
-    if ((window.pageYOffset) >= 800) {
-        document.getElementById("pic1").style.opacity = "1"; 
-    } else {
-        document.getElementById("pic1").style.opacity = "0"; 
+window.onscroll = function () {
+  if ((window.pageYOffset) >= 1900) {
+    document.getElementById("pic1").style.opacity = "1";
+    if ((window.pageYOffset) >= 3000) {
+      document.getElementById("video1").style.opacity = "1";
     }
+      if ((window.pageYOffset) >= 3500) {
+        document.getElementById("pic2").style.opacity = "1";
+      }
+
+  }  
+  else {
+    document.getElementById("pic1").style.opacity = "0";
+    document.getElementById("video1").style.opacity = "0";
+    document.getElementById("pic2").style.opacity = "0";
+  }
 }
 
 
 
 
 // Load this when the DOM is ready
-$(function(){
+$(function () {
   // You used .myCarousel here. 
   // That's the class selector not the id selector,
   // which is #myCarousel
   $('#myCarousel').carousel();
 });
+
+
+
